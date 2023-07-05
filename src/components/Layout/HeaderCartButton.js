2eb8d1e2a63ab4142a44444c1364f2d228bdb9bc
Alaa Bolbol <72712987+AlaaBol@users.noreply.github.com>
2023-07-05 13:30:19 +0200

    Initial commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import CaerIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CaerIcon />
+      </span>
+      <span> Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
+export default HeaderCartButton;
